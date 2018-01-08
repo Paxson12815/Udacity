b331f30a356f67f6086b15dc35f1f922f92767a9
Paxson12815 <33859457+Paxson12815@users.noreply.github.com>
2018-01-08 08:45:32 -0500

    Add files via upload

--- FeedReader Udacity/jasmine/spec/feedreader.js
+++ FeedReader Udacity/jasmine/spec/feedreader.js
@@ -28,7 +28,7 @@ $(function() {
     describe('The Menu', function() {
         //Write a test that ensures the menu element is hidden by default. You'll have to analyze the HTML and the CSS to determine how we're performing the hiding/showing of the menu element.
         it('Menu element is hidden by default', function() {
-            expect($('body').hasClass('menu-hidden')).toBeTruthy()
+            expect($('body').hasClass('menu-hidden')).toBeTruthy();
         });
         //Write a test that ensures the menu changes visibility when the menu icon is clicked. This test should have two expectations: does the menu display when clicked and does it hide when clicked again.
         it('Menu changes visibility when clicked with two expectations', function() {
